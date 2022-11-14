5dc759037c57aa207c97a886a9c865cfc48883be
CptPenguin <32851658+CptPenguin@users.noreply.github.com>
2022-11-14 18:04:12 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Import stylesheets
 import './style.css';
+import data from './JSONlog-0-7-A0.json';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+readJson();
+
+function readJson() {
+  fetch(data)
+    .then((response) => response.json)
+    .then((json) => console.log(json));
+}
